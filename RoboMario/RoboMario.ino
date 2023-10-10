d9171aa3dcfdfd3db3e525618feeb239cb1e61b7
eltonssilva <eltonssilva@yahoo.com.br>
2023-10-10 16:13:37 -0300

    Criado esquelto movimento robo

--- RoboMario/RoboMario.ino
+++ RoboMario/RoboMario.ino
@@ -1,9 +1,33 @@
+
+
+#define motor_d_1 12
+#define motor_d_2 12
+
+#define motor_e_1 12
+#define motor_e_2 12
+
+
+
 void setup() {
   // put your setup code here, to run once:
+
+pinMode(motor_d_1, OUTPUT);
+
+digitalWrite(motor_d_1, 0);
+digitalWrite(motor_d_1, 0);
+digitalWrite(motor_d_1, 0);
+digitalWrite(motor_d_1, 0);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  robot_front();
+  delay(5000);
+  robot_stop();
+  delay(2000);
+  robot_back();
+
 }
+
---
+++ RoboMario/controle_robo.ino
@@ -0,0 +1,48 @@
+
+/*
+
+Frente
+motor_d_1 = 1
+motor_d_2 = 0
+motor_e_1 = 1
+motor_e_2 = 0
+
+Tras
+motor_d_1 = 0
+motor_d_2 = 1
+motor_e_1 = 0
+motor_e_2 = 1
+
+
+Stop
+motor_d_1 = 0
+motor_d_2 = 0
+motor_e_1 = 0
+motor_e_2 = 0
+
+
+*/
+
+void robot_front(){
+
+
+
+}
+
+void robot_back(){
+
+}
+
+void robot_stop(){
+
+}
+
+
+void robot_left(){
+
+}
+
+
+void robot_right(){
+
+}
