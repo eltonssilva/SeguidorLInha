a8e9f83df10d70f9414d78aa9f38f04bf7ad862a
LucioGB <luciogabrielbraga18@gmail.com>
2023-10-16 16:44:26 -0300

    Comands

--- RoboMario/RoboMario.ino
+++ RoboMario/RoboMario.ino
@@ -1,10 +1,10 @@
 
 
-#define motor_d_1 12
-#define motor_d_2 12
+#define motor_d_1 5
+#define motor_d_2 6
 
-#define motor_e_1 12
-#define motor_e_2 12
+#define motor_e_1 10
+#define motor_e_2 11
 
 
 
@@ -26,10 +26,15 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   robot_front();
-  delay(5000);
+  delay(2000);
   robot_stop();
   delay(2000);
   robot_back();
+  delay(2000);
+  robot_left();
+  delay(2000);
+  robot_right();
+  delay(2000);
 
 }
 
--- RoboMario/controle_robo.ino
+++ RoboMario/controle_robo.ino
@@ -1,7 +1,4 @@
-
-/*
-
-Frente
+/*Frente
 motor_d_1 = 1
 motor_d_2 = 0
 motor_e_1 = 1
@@ -19,31 +16,52 @@ motor_d_1 = 0
 motor_d_2 = 0
 motor_e_1 = 0
 motor_e_2 = 0
-
-fff
-
 */
+
 
 void robot_front(){
 
-
-
+  digitalWrite(motor_d_1, HIGH);
+  digitalWrite(motor_d_2, LOW);
+  digitalWrite(motor_e_1, HIGH);
+  digitalWrite(motor_e_2, LOW);
+  
 }
 
 void robot_back(){
+
+  digitalWrite(motor_d_1, LOW);
+  digitalWrite(motor_d_2, HIGH);
+  digitalWrite(motor_e_1, LOW);
+  digitalWrite(motor_e_2, HIGH);
 
 }
 
 void robot_stop(){
 
+  digitalWrite(motor_d_1, LOW);
+  digitalWrite(motor_d_2, LOW);
+  digitalWrite(motor_e_1, LOW);
+  digitalWrite(motor_e_2, LOW);
+  
 }
 
 
 void robot_left(){
 
+  digitalWrite(motor_d_1, HIGH);
+  digitalWrite(motor_d_2, HIGH);
+  digitalWrite(motor_e_1, LOW);
+  digitalWrite(motor_e_2, LOW);
+
 }
 
 
 void robot_right(){
+
+  digitalWrite(motor_d_1, LOW);
+  digitalWrite(motor_d_2, LOW);
+  digitalWrite(motor_e_1, HIGH);
+  digitalWrite(motor_e_2, HIGH);
 
 }
